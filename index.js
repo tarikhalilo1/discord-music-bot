3f71de671e13c6fe1dff159b2146dd38ff870708
tarikhalilo1 <kyngtarik12@gmail.com>
2026-02-21 13:50:23 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log("Bot çalışıyor!");
+});
+
+client.login(process.env.BOT_TOKEN);
